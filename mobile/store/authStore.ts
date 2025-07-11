726a232df116292928cd0625b7440849ca4556ae
Nhatnguyen-N <nhatnguyen13213@gmail.com>
2025-07-11 16:42:19 +0700

    cron.js added.

--- mobile/store/authStore.ts
+++ mobile/store/authStore.ts
@@ -14,7 +14,7 @@ export const useAuthStore = create<AuthStore>((set) => ({
   register: async (username: string, email: string, password: string) => {
     set({ isLoading: true });
     try {
-      const response = await fetch("http://192.168.1.7:3000/api/auth/register", {
+      const response = await fetch("https://book-store-app-kwvc.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
